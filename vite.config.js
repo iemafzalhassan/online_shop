e9d18abcdeb642bf41fb53fd6258858dacfa31ef
Talha Aziz <aziz81812@gmail.com>
2025-02-20 14:06:53 +0500

    fix: add vite-plugin-node-polyfills for Node.js compatibility

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import nodePolyfills from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), nodePolyfills()],
+  plugins: [
+    react(),
+    nodePolyfills()  // Apply polyfills plugin
+  ],
   base: './',
   css: {
     devSourcemap: false
