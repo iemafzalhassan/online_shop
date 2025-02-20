9db077e36dd6a917be590b2d7f84e586131af154
Ubuntu <ubuntu@ip-172-31-22-94.ec2.internal>
2025-02-20 20:36:37 +0000

    Added Dockify.md and Dockerfile, updated vite.config.js for Docker support

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    base: './',
-    css: {
-      devSourcemap: false
-    }
-})
+  plugins: [react()],
+  base: './',
+  css: {
+    devSourcemap: false
+  },
+  server: {
+    host: '0.0.0.0',  // Allow access from outside the container
+    port: 3000        // Set port to 3000
+  }
+});
+
