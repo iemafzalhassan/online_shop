e0d44c49361a92e7bcb8b613c57dd7a8e656eae0
Ubuntu <ubuntu@ip-172-31-89-219.ec2.internal>
2025-02-21 00:22:57 +0000

    feat: Added a console update file

---
+++ linux.sh
@@ -0,0 +1,5 @@
+sudo apt update #update the system 
+sudo apt uprgrade #upgrade the packages
+sudo apt-get install net-tools #install networking tools
+echo "System update, upgrade and file installion complete"
+~                                                            
