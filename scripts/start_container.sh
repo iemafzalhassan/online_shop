3f1225581fcee7940fb0c1b0d613a7e8c072a096
deepakyelwande15 <107405365+deepakyelwande15@users.noreply.github.com>
2025-04-27 14:44:58 +0530

    file changed

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,5 +4,5 @@
   # Pull the latest Docker image from Docker Hub
   docker pull trainwithshubham/tws-online-shop-app-demo:latest
 
-  # Run the Docker image as a container
+  # Run the Docker image as a container.
   docker run -d -p 5173:5173 trainwithshubham/tws-online-shop-app-demo:latest
